6052ade0534cdd47986392844d0fb46255f9256d
Henrique Buzin <cultura.henrique@gmail.com>
2018-06-14 16:31:10 -0300

    Primeira Versão

---
+++ assets/js/index.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+	var boost = $("#boost");
+	var buck = $("#buck");
+	var rectifier = $("#rectifier");
+	$(".tabs").tabs();
+	$(".circuit").click(function(){ 
+		var value = $(".circuit:checked").val();
+		if(value == 1) {
+			boost.slideUp("slow");
+			buck.slideUp("slow");
+			rectifier.slideDown("slow");
+		} else if(value == 2) {
+			rectifier.slideUp("slow");
+			boost.slideUp("slow");
+			buck.slideDown("slow");
+		} else {
+			rectifier.slideUp("slow");
+			buck.slideUp("slow");
+			boost.slideDown("slow");
+		}
+	});
+	$("input[type=\"number\"]").keydown(function (event) {
+        if (event.shiftKey == true) {
+            event.preventDefault();
+        }
+        if (!((event.keyCode >= 48 && event.keyCode <= 57) || (event.keyCode >= 96 && event.keyCode <= 105) || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 37 || event.keyCode == 39 || event.keyCode == 46 || event.keyCode == 190)) {
+            event.preventDefault();
+        }
+        if($(this).val().indexOf(',') !== -1 && event.keyCode == 190) {
+			event.preventDefault();
+		}
+    });
+});
